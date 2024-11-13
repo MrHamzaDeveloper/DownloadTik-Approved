a2880a470773098cedd52f69670098ec8814315d
Hamza Rasool <hamzarasool585@gmail.com>
2024-11-13 12:27:02 +0500

    Robots.txt

--- src/app/page.js
+++ src/app/page.js
@@ -15,10 +15,7 @@ const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
 export default function Page() {
   return (
     <>
-      <Suspense fallback={<div>Loading Header...</div>}>
-        <Header />
-      </Suspense>
-      
+      <Header />      
       {/* Main content with hydration warning suppression */}
       <div className="container mx-auto py-10 px-4 flex flex-col items-center" suppressHydrationWarning>
         <Typewriter text="Download TikTok video without watermark in HD Quality" />
@@ -27,9 +24,9 @@ export default function Page() {
         <FAQs />
       </div>
 
-      <Suspense fallback={<div>Loading Footer...</div>}>
+      
         <Footer />
-      </Suspense>
+      
     </>
   );
 }
